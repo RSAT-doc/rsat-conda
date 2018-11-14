4ff897954846ff216f211b4b533ffb928f6fac20
Aurelien Naldi <aurelien.naldi@gmail.com>
2018-11-14 17:02:57 +0100

    start rsat itself

--- rsat-core/build.sh
+++ rsat-core/build.sh
@@ -1,3 +1,20 @@
 
-# TODO: build binaries and put everything in the right place
 
+# TODO: better file system layout and entry point command
+RSAT_DEST="$PREFIX/opt/rsat/"
+mkdir -p "$RSAT_DEST"
+
+cp -a perl-scripts python-scripts "$RSAT_DEST"
+
+
+# Build and dispatch compiled binaries
+cd contrib
+for dbin in *
+do
+	cd "$dbin"
+	make clean && make && cp "$dbin" "$PREFIX/bin"
+	cd ..
+done
+
+
+# TODO: R packaging
