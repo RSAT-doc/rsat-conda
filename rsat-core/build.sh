3b35f38afc3861e76d90b4490e09a2980ca14c55
Jacques van Helden <Jacques.van-Helden@univ-amu.fr>
2019-06-26 14:34:22 +0200

    added rsat.yamlin the package + write a small shell rsat script calling the rsat python script

--- rsat-core/build.sh
+++ rsat-core/build.sh
@@ -4,8 +4,15 @@
 RSAT_DEST="$PREFIX/opt/rsat/"
 mkdir -p "$RSAT_DEST"
 
-cp -a perl-scripts python-scripts makefiles rsat "$RSAT_DEST"
+cp -a perl-scripts python-scripts makefiles rsat rsat.yaml "$RSAT_DEST"
 
+echo "#!/bin/sh" > $PREFIX/bin/rsat
+echo "../opt/rsat/rsat" >> $PREFIX/bin/rsat
+chmod 755 $PREFIX/bin/rsat
+
+## Alternative: 
+## - au opt: placer les scripts perl, le yaml etc dans usr/share/rsat
+## - au dispatcher dans bin: placer dans le bin le script python rsat et lui indiquer où aller pêcher le yaml.
 
 # Build and dispatch compiled binaries
 cd contrib
